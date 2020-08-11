573fbc0d6a72286538e1c9f47ccfb5accf7c8466
Yunhao Gao <as510891328@gmail.com>
2020-08-11 03:55:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,6 +6,6 @@ def join_nested_strings(src)
   while src[row] do
     col = 0
     while src[row][col] do
-      if src[row][col].Class == string 
+      if src[row][col].is_a?(String)
         
 end
